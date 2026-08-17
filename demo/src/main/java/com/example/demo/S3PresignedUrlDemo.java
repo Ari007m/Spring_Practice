7b9ef6104101c4d9c75ca5429b4536e9f1295f60
Ari007m <ariharasudan007@gmail.com>
2026-08-17 11:07:03 +0530

    S3 presigned url test

---
+++ demo/src/main/java/com/example/demo/S3PresignedUrlDemo.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
+import software.amazon.awssdk.regions.Region;
+import java.time.Duration;
+import java.net.URL;
+
+public class S3PresignedUrlDemo {
+    @Value("${aws.s3.bucket}")
+    private static String bucket;
+    public static void main(String[] args) {
+        try (S3Presigner presigner = S3Presigner.builder()
+                .region(Region.US_EAST_1) // change to your bucket's actual region
+                .build()) {
+
+            GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                    .bucket(bucket)
+                    .key("dummy.txt")
+                    .build();
+
+            GetObjectPresignRequest presignRequest = GetObjectPresignRequest.builder()
+                    .signatureDuration(Duration.ofMinutes(15))
+                    .getObjectRequest(getObjectRequest)
+                    .build();
+
+            PresignedGetObjectRequest presignedRequest = presigner.presignGetObject(presignRequest);
+            URL url = presignedRequest.url();
+
+            System.out.println("Presigned URL: " + url);
+            System.out.println("Generated at: " + java.time.LocalDateTime.now());
+        }
+    }
+}
